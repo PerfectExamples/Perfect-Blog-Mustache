072aa611c078729429e73001671c80006f092dc0
Konstantin Smirnov <covox@covox.ru>
2019-05-27 20:29:48 +0300

    Изменения, которые будут включены в коммит: 	изменено:      Package.swift 	изменено:      Sources/main.swift Swift 4up update

--- Package.swift
+++ Package.swift
@@ -1,13 +1,18 @@
+// swift-tools-version:5.0
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-10-10 14:39:41 +0000
 import PackageDescription
 let package = Package(
 	name: "Perfect-Blog-Mustache",
-	targets: [],
-	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
-		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
+		dependencies: [
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.0"),
+		.package(url: "https://github.com/iamjono/SwiftString.git", from: "2.0.0"),
+	],
+	targets: [
+		.target(name: "Perfect-Blog-Mustache", 
+		dependencies: ["PerfectHTTPServer","PerfectSQLite","PerfectMustache","SwiftString"],
+		path: "Sources")
 	]
 )
